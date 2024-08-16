b8506d738c4a1757a07e35e2785cd64ba0a97b43
monirulislamkhan <mr.monirulkhan@gmail.com>
2024-08-16 23:38:04 +0530

    added if else statement

---
+++ ifelse/js/main.js
@@ -0,0 +1,13 @@
+let allowed;
+// let age = 10;
+let age = prompt("What is your old");
+/* if (age > 10) {
+  allowed = true;
+} else {
+  allowed = false;
+}
+console.log(allowed);
+console.log(typeof allowed); */
+age > 10 ? (allowed = true) : (allowed = false);
+console.log(allowed);
+console.log(typeof allowed);
